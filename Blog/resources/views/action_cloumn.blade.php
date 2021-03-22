6c9472baa82018b8eb0e91e46cb6b863378925d0
alperncetn <alperncetn>
2021-03-22 10:40:51 +0300

    deneme 2

--- Blog/resources/views/action_cloumn.blade.php
+++ Blog/resources/views/action_cloumn.blade.php
@@ -2,5 +2,4 @@
 <div>
 <a href="#"  onclick="edit({{$model->id}})" value="' . $row->id . '" data-rowid="' . $row->id . '" class="btn btn-xs btn-secondary">Düzenle</a>
 <button onclick="del({{$model->id}})" class="btn btn-xs btn-danger">Sil</button>
-<button onclick="del({{$model->id}})" class="btn btn-xs btn-danger">Sil</button>
 </div>
